24a367ba5a5553353e32d7b814b2d00ba3608a9a
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2018-01-13 15:50:01 -0600

    Removed deprecated logger.Help

--- logger/main.go
+++ logger/main.go
@@ -10,33 +10,6 @@ var (
 	Muted bool
 )
 
-func Help(root string, semaphoreSize int) {
-	fmt.Printf(
-		`rp <options> <file/directory>
--o="": (old)
-      string in file to replace
--n="": (new)
-      string to replace old string with
--x="": (exclude)
-      Patterns to exclude from matches, separated by commas
--e=false: (expression)
-      Treat '-o' and '-n' as regular expressions
--r=false: (recursive)
-      Edit matching files recursively [down to the bottom of the directory]
--d="%s": (directory)
-      Directory under which to edit files recursively
--s="%d": (semaphore-size)
-      Max number of files to edit at the same time
-      WARNING: Setting this too high will cause the program to crash,
-      corrupting the files it was editing
--q=false: (quiet)
-      Don't list edited files
--Q=false: (Quiet)
-      Don't show any output at all`,
-		root, semaphoreSize,
-	)
-}
-
 func Progress(path string) {
 	if Quiet || Muted {
 		return
