85be0d46b43d1b7f71c51925daee82ee4ce7f1b8
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2018-01-14 17:57:24 -0600

    DRY

--- replacer/main_test.go
+++ replacer/main_test.go
@@ -7,16 +7,17 @@ import (
 
 var ()
 
-// TestReplace tests r.replace without regex
-func TestReplaceNonRegex(t *testing.T) {
+// TestReplace tests r.replace
+func TestReplace(t *testing.T) {
+
+	contents := []byte(`<svg fill="#4caf50" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#4caf50"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#4caf50"/></svg>`)
+	expected := []byte(`<svg fill="#f44336" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#f44336"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#f44336"/></svg>`)
 
 	t.Run("r.replace (non-regex)", func(t *testing.T) {
-		contents := []byte(`<svg fill="#4caf50" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#4caf50"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#4caf50"/></svg>`)
 		toFind := "#4caf50"
 		toReplace := "#f44336"
 		regex := false
 		r := New(toFind, toReplace, regex)
-		expected := []byte(`<svg fill="#f44336" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#f44336"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#f44336"/></svg>`)
 
 		replaced := r.replace(contents)
 		if !bytes.Equal(replaced, expected) {
@@ -24,19 +25,13 @@ func TestReplaceNonRegex(t *testing.T) {
 			return
 		}
 	})
-}
-
-// TestReplaceRegex tests r.replace with regex
-func TestReplaceRegex(t *testing.T) {
 
 	t.Run("r.replace (regex)", func(t *testing.T) {
-		contents := []byte(`<svg fill="#4caf50" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#4caf50"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#4caf50"/></svg>`)
 		toFind := `(fill=").*?(")`
 		toReplace := `$1#f44336$2`
 		regex := true
 		r := New(toFind, toReplace, regex)
 
-		expected := []byte(`<svg fill="#f44336" height="48" viewBox="0 0 48 48" width="48" xmlns="http://www.w3.org/2000/svg"><path d="M28.93 27L22 40V29h-4l1.07-2H14v14.33C14 42.8 15.19 44 16.67 44h14.67c1.47 0 2.67-1.19 2.67-2.67V27h-5.08z" fill="#f44336"/><path d="M31.33 8H28V4h-8v4h-3.33C15.19 8 14 9.19 14 10.67V27h5.07L26 14v11h4l-1.07 2H34V10.67C34 9.19 32.81 8 31.33 8z" fill-opacity=".3" fill="#f44336"/></svg>`)
 		replaced := r.replace(contents)
 		if !bytes.Equal(replaced, expected) {
 			t.Errorf("Expected `replaced` to be\n%s\nbut got\n%s.\n", expected, replaced)
