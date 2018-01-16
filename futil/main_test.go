c157fd87ce970a018415bb1dc8abc6516dc76341
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2018-01-16 14:29:24 -0600

    Updated test

--- futil/main_test.go
+++ futil/main_test.go
@@ -6,16 +6,16 @@ import (
 
 var ()
 
-// TestFilterSymlinks tests FilterSymlinks
-func TestFilterSymlinks(t *testing.T) {
+// TestHardlinks tests Hardlinks
+func TestHardlinks(t *testing.T) {
 
 	matches := []string{"../.test_tmp/battery-050-charging.svg", "../.test_tmp/dir1/file1.svg", "../.test_tmp/dir1/dir2/file2-link.svg", "../.test_tmp/dir1/dir2/file2.svg", "../.test_tmp/dir1/dir2/terminal.svg"}
 	expected := []string{"../.test_tmp/battery-050-charging.svg", "../.test_tmp/dir1/file1.svg", "../.test_tmp/dir1/dir2/file2.svg", "../.test_tmp/dir1/dir2/terminal.svg"}
 
 	t.Run("Glob (recursive)", func(t *testing.T) {
-		filtered := FilterSymlinks(matches)
-		if !slcEquals(filtered, expected) {
-			t.Errorf("Expected `filtered` to be %v but got %v.\n", expected, filtered)
+		hardlinks := Hardlinks(matches)
+		if !slcEquals(hardlinks, expected) {
+			t.Errorf("Expected `hardlinks` to be %v but got %v.\n", expected, hardlinks)
 			return
 		}
 	})
