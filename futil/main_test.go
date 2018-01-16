40722fd3f9b81caab317a66b4dc362c984aa74e8
GreenRaccoon23 <GreenRaccoon23@gmail.com>
2018-01-16 14:29:42 -0600

    Fixed test msg

--- futil/main_test.go
+++ futil/main_test.go
@@ -12,7 +12,7 @@ func TestHardlinks(t *testing.T) {
 	matches := []string{"../.test_tmp/battery-050-charging.svg", "../.test_tmp/dir1/file1.svg", "../.test_tmp/dir1/dir2/file2-link.svg", "../.test_tmp/dir1/dir2/file2.svg", "../.test_tmp/dir1/dir2/terminal.svg"}
 	expected := []string{"../.test_tmp/battery-050-charging.svg", "../.test_tmp/dir1/file1.svg", "../.test_tmp/dir1/dir2/file2.svg", "../.test_tmp/dir1/dir2/terminal.svg"}
 
-	t.Run("Glob (recursive)", func(t *testing.T) {
+	t.Run("Hardlinks", func(t *testing.T) {
 		hardlinks := Hardlinks(matches)
 		if !slcEquals(hardlinks, expected) {
 			t.Errorf("Expected `hardlinks` to be %v but got %v.\n", expected, hardlinks)
